6381e828962693224e332b9f8de924f4e8da487b
striado <s.triado@sapalomera.cat>
2023-05-11 09:31:49 +0200

    Creació ProvinciaDAODB
    Creació classe Provincia DAODB per implementar els mètodes de la interfície DAO

---
+++ JDBC-DAO/src/MODELO/ProvinciaDAODB.java
@@ -0,0 +1,143 @@
+package MODELO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProvinciaDAODB implements DAODB<Provincia>{
+    @Override
+    public boolean create(Provincia c) {
+        String query = "INSERT INTO provincies (provincia_id,comunitat_aut_id,nom,codi_ine,num_escons) " +
+                "VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            preparedStmt.setInt(1, c.getId());
+            preparedStmt.setInt(2, c.getCA());
+            preparedStmt.setString(3, c.getNom());
+            preparedStmt.setString(4, c.getCodi_ine());
+            preparedStmt.setInt(5, c.getNumEscons());
+            preparedStmt.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    @Override
+    public Provincia read(int id) {
+        String query = "SELECT * FROM provincies WHERE provincia_id = ?";
+        Provincia c = new Provincia();
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            preparedStmt.setInt(1, id);
+
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next()) {
+                c.setId(rs.getInt("provincia_id"));
+                c.setCA(rs.getInt("comunitat_aut_id"));
+                c.setNom(rs.getString("nom"));
+                c.setCodi_ine(rs.getString("codi_ine"));
+                c.setNumEscons(rs.getInt("num_escons"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return c;
+    }
+
+    @Override
+    public boolean update(Provincia c, int id) {
+        String query = "UPDATE provincies SET provincia_id = ?, nom = ?, codi_ine = ? WHERE comunitat_aut_id=?";
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            preparedStmt.setInt(1, c.getId());
+            preparedStmt.setString(2, c.getNom());
+            preparedStmt.setString(3, c.getCodi_ine());
+            preparedStmt.setInt(4, id);
+            preparedStmt.execute();
+
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String query = "DELETE FROM provincies WHERE provincia_id = ?";
+
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            preparedStmt.setInt(1, id);
+            preparedStmt.execute();
+
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean exists(int id) {
+        String query = "SELECT * FROM provincies WHERE provincia_id = ?";
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            preparedStmt.setInt(1, id);
+
+            ResultSet rs = preparedStmt.executeQuery();
+            return rs.next();
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    @Override
+    public int count() {
+        String query = "SELECT COUNT(*) FROM provincies";
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            } else return 0;
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    @Override
+    public List<Provincia> all() {
+        List<Provincia> registros = new ArrayList<>();
+        String query = "SELECT * FROM provincies";
+        try {
+            PreparedStatement preparedStmt = JDBC.con.prepareStatement(query);
+            ResultSet rs = preparedStmt.executeQuery();
+            while (rs.next()) {
+                Provincia c = new Provincia();
+                c.setId(rs.getInt("provincia_id"));
+                c.setCA(rs.getInt("comunitat_aut_id"));
+                c.setNom(rs.getString("nom"));
+                c.setCodi_ine(rs.getString("codi_ine"));
+                c.setNumEscons(rs.getInt("num_escons"));
+                registros.add(c);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return registros;
+    }
+
+}
